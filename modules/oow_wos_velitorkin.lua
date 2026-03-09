756079140b76168875d80ee2041e5fa35c76ca26
Nate Dahl <nndahl@gmail.com>
2026-03-09 11:26:17 -0500

    Modified velitorkin

--- modules/oow_wos_velitorkin.lua
+++ modules/oow_wos_velitorkin.lua
@@ -1,23 +1,17 @@
 local mq = require('mq')
 
 local RaidBoss = require('AutoBot.lib.raidboss')
-local BossRoles = require('AutoBot.lib.bossroles')
 
 local M = {}
 
 M.id = "oow_wos_velitorkin"
 
+local boss = RaidBoss.new(M.id, "Velitorkin")
+
 M.help = boss.standard_help({
   title = 'Omens of War - Wall of Slaughter - Velitorkin',
   include_ot = false,
 })
-
-local boss = RaidBoss.new(M.id, "Velitorkin")
-
-local function me_name()
-  local n = mq.TLO.Me.Name()
-  return (type(n) == 'function') and n() or n
-end
 
 local function say(msg)
   mq.cmdf('/dga /echo [Velitorkin:%s] %s', mq.TLO.Me.Name() or 'Me', msg)
@@ -25,7 +19,6 @@ end
 
 function M.start(ctx)
   boss.start()
-
   local summary = boss.role_summary()
   if summary then
     say("Role loaded: " .. summary)
@@ -34,7 +27,6 @@ end
 
 function M.stop(ctx)
   boss.stop()
-
   local summary = boss.role_summary()
   if summary then
     say("Module stopped. Role=" .. summary)
@@ -45,93 +37,11 @@ function M.tick(ctx)
   boss.tick()
 end
 
-M.commands = {}
-
-M.commands.startfight = function()
-  boss.start_fight()
-  say("Fight started.")
-end
-
-M.commands.stopfight = function()
-  boss.stop_fight()
-  say("Fight stopped.")
-end
-
-M.commands.mtset = function()
-  local name = BossRoles.set_mt(M.id)
-  boss.reload_roles()
-  say("MT set to " .. tostring(name))
-end
-
-M.commands.bmtset = function()
-  local name = BossRoles.set_bmt(M.id)
-  boss.reload_roles()
-  say("BMT set to " .. tostring(name))
-end
-
-M.commands.maset = function()
-  local name = BossRoles.set_ma(M.id)
-  boss.reload_roles()
-  say("MA set to " .. tostring(name))
-end
-
-M.commands.mtclear = function()
-  BossRoles.clear_mt(M.id)
-  boss.reload_roles()
-  say("MT cleared")
-end
-
-M.commands.bmtclear = function()
-  BossRoles.clear_bmt(M.id)
-  boss.reload_roles()
-  say("BMT cleared")
-end
-
-M.commands.maclear = function()
-  BossRoles.clear_ma(M.id)
-  boss.reload_roles()
-  say("MA cleared")
-end
-
-M.commands.rtadd = function()
-  local name = BossRoles.add_rampage_tank(M.id)
-  boss.reload_roles()
-  say("Rampage tank added " .. tostring(name))
-end
-
-M.commands.rtdel = function()
-  local name = BossRoles.remove_rampage_tank(M.id)
-  boss.reload_roles()
-  say("Rampage tank removed " .. tostring(name))
-end
-
-M.commands.rtclear = function()
-  BossRoles.clear_rampage_tanks(M.id)
-  boss.reload_roles()
-  say("All rampage tanks cleared")
-end
-
-M.commands.rtstatus = function()
-  local r = BossRoles.get_rampage_tanks(M.id)
-  say("RT: " .. (#r > 0 and table.concat(r, ", ") or "none"))
-end
-
-M.commands.tankstatus = function()
-  say(
-    "MT=" .. tostring(BossRoles.get_mt(M.id)) ..
-    " BMT=" .. tostring(BossRoles.get_bmt(M.id)) ..
-    " MA=" .. tostring(BossRoles.get_ma(M.id))
-  )
-end
-
-M.commands.mastatus = function()
-  local ma = BossRoles.get_ma(M.id)
-  local mine = BossRoles.is_ma(M.id, me_name())
-  say(
-    "MA=" .. tostring(ma) ..
-    " me=" .. tostring(me_name()) ..
-    " is_ma=" .. tostring(mine)
-  )
-end
+M.commands = boss.merge_commands(
+  boss.standard_commands(M, say),
+  boss.standard_role_commands(say, {
+    include_ot = false,
+  })
+)
 
 return M
